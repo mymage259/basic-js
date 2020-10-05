5b8af2c6f41b04ccb81b22347478985da637691d
Aleksandr <70815958+mymage259@users.noreply.github.com>
2020-10-05 22:07:30 +0300

    feat: update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,87 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+let array = arr;
+let arrayTest = [];
+let count = 0;
+let index;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === '--discard-next' || arr[i] === '--discard-prev' || arr[i] === '--double-next' || arr[i] === '--double-prev') {
+    count++;
+  }
+}
+for (let i = 0; i < count; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] === '--discard-next') {
+      for (let k = 0; k < array.length; k++) {
+        if (k !== j) {
+          arrayTest.push(array[k]);
+        }
+        else {
+          if (j !== array.length - 1 && array[j + 1] !== null) {
+            arrayTest.push(null);
+            k++;
+          }
+        }
+      }
+      array = arrayTest;
+      arrayTest = [];
+      break;
+    } 
+    else if (array[j] === '--discard-prev') {
+      for (let k = 0; k < array.length; k++) {
+        if (k !== j) {
+          arrayTest.push(array[k]);
+        }
+        else {
+          if (j !== 0 && array[j - 1] !== null) {
+            arrayTest.pop();
+            arrayTest.push(null);
+          }
+        }
+      }
+      array = arrayTest;
+      arrayTest = [];
+      break;
+    }
+    else if (array[j] === '--double-next') {
+      for (let k = 0; k < array.length; k++) {
+        if (k !== j) {
+          arrayTest.push(array[k]);
+        }
+        else {
+          if (j !== array.length - 1 && array[j + 1] !== null) {
+            arrayTest.push(array[k + 1]);
+          }
+        }
+      }
+      array = arrayTest;
+      arrayTest = [];
+      break;
+    } 
+    else if (array[j] === '--double-prev') {
+      for (let k = 0; k < array.length; k++) {
+        if (k !== j) {
+          arrayTest.push(array[k]);
+        }
+        else {
+          if (j !== 0 && array[j - 1] !== null) {
+            arrayTest.push(array[k - 1]);
+          }
+        }
+      }
+      array = arrayTest;
+      arrayTest = [];
+      break;
+    }
+  }
+}
+for (let i = 0; i < array.length; i++) {
+  if (array[i] !== null) {
+    arrayTest.push(array[i]);
+  }
+}
+array = arrayTest;
+return array;
+}; 
