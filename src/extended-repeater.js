461c6077a4c6dfde426982eceaabf566f38b5d98
Aleksandr <70815958+mymage259@users.noreply.github.com>
2020-10-12 21:05:51 +0300

    feat: update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  // str - строка
+  // options объяект содержит строки или нужно преобразовать к строкам
+  // options.repeatTimes - число с указанием количества повторений строки
+  // options.separator - строка содержит разделители повторений строки по умолчанию '+'
+  // options.addition - приписка к строке, может отсутствовать
+  // options.additionRepeatTimes - число сколько раз повторить приписку к строке
+  // options.additionSeparator - строка содержит разделители повторений приписок к строке по умолчанию '|'
+  if (str === null) {
+    str = 'null';
+  }
+  let repeatTimes = options.repeatTimes;
+  if (repeatTimes === undefined) {
+    repeatTimes = 1;
+  }
+  let separator = options.separator;
+  if (separator === undefined) {
+    separator = '+';
+  }
+  let addition = options.addition;
+  if (addition === undefined) {
+    addition = '';
+  }
+  if (addition === null) {
+    addition = 'null';
+  }
+  let additionRepeatTimes = options.additionRepeatTimes; 
+  if (additionRepeatTimes === undefined) {
+    additionRepeatTimes = 1;
+  }
+  let additionSeparator = options.additionSeparator;
+  if (additionSeparator === undefined) {
+    additionSeparator = '|';
+  }
+  let arrayAddition = [];
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    arrayAddition.push(addition);
+  }
+  let mainStr = str + arrayAddition.join(additionSeparator);
+  let arrayStr = [];
+  for (let i = 0; i < repeatTimes; i++) {
+    arrayStr.push(mainStr);
+  }
+  return arrayStr.join(separator);
+
 };
   
